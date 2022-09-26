d1477540c96cd0415ff0cf454d150cb4440b417a
moradilahroodi <ssn.moradi@gmail.com>
2022-09-26 16:34:47 +0330

    Add Color Form

--- src/ColorApp.js
+++ src/ColorApp.js
@@ -1,21 +1,42 @@
 import React, { useState } from "react";
 import Colordata from "./data/color-data.json";
 import Colorlist from "./components/Colorlist";
+import AddColorForm from "./components/AddColorForm";
+import { v4 } from "uuid";
 
 const ColorApp = () => {
     const [colors, setColors] = useState(Colordata);
-    return <Colorlist
-        colors={colors}
-        onRateColor={(id, rating) => {
-            const newColors = colors.map( color =>
-                color.id === id ? { ...color, rating } : color
-            );
-            setColors(newColors);
-        }}
-        onRemoveColor={id => {
-            const newColors = colors.filter(color => color.id !== id);
-            setColors(newColors);
-        }}
-    />
+    return (
+        <>
+            <AddColorForm
+                onNewColor={(title, color)=>{
+                    const newColors = [
+                        ...colors,
+                        {
+                            id: v4(),
+                            rating: 0,
+                            title,
+                            color
+                        }
+                    ];
+                    setColors(newColors);
+                }}
+            />
+            <Colorlist
+                colors={colors}
+                onRateColor={(id, rating) => {
+                    const newColors = colors.map(color =>
+                        color.id === id ? { ...color, rating } : color
+                    );
+                    setColors(newColors);
+                }}
+                onRemoveColor={id => {
+                    const newColors = colors.filter(color => color.id !== id);
+                    setColors(newColors);
+                }}
+            />
+        </>
+    )
+
 }
 export default ColorApp;
--- src/components/AddColorForm.js
+++ src/components/AddColorForm.js
@@ -1,20 +1,20 @@
-import { useRef } from "react";
+import {useInput} from "./hooks/useInput";
 
-export default function AddColorForm(onNewColor = f => f) {
-    const txtTitle = useRef();
-    const hexColor = useRef();
-    const submit = e => {
-        e.preventDefault();
-        const title = txtTitle.current.value;
-        const color = hexColor.current.value;
-        onNewColor(title, color);
-        txtTitle.current.value = "";
-        hexColor.current.value = "";
-    };
+export default function AddColorForm({onNewColor}) {
+
+    const [titleProps, resetTitle] = useInput("");
+    const [colorProps, resetColor] = useInput("#000000");
+
+    const submit = event => {
+        event.preventDefault();
+        onNewColor(titleProps.value, colorProps.value);
+        resetTitle();
+        resetColor();
+    }
     return (
         <form onSubmit={submit}>
-            <input ref={txtTitle} type="text" placeholder="Color title ..." required />
-            <input ref={hexColor} type="color" required />
+            <input {...titleProps} type="text" placeholder="Color title ..." required />
+            <input {...colorProps} type="color" required />
             <button>ADD</button>
         </form>
     )
